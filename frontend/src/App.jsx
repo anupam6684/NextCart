c24bedb0f270c778fb018b8d3390f9b79a5e1bfa
Anupam Jana <anupamjana6684@gmail.com>
2025-12-30 17:18:34 +0530

    install some library

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+function App() {
+  return (
+    <>
+      <div>
+        <p className="fs-1 fw-bold">
+          Nextcart <i class="fa-solid fa-cart-arrow-down"></i>
+        </p>
+        <ShoppingCartIcon />
+      </div>
+    </>
+  );
+}
+
+export default App;
